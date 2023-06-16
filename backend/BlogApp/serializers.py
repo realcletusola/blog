9ccba33cc0433b227e31f0d2281d2c73b508e6bc
cletusola <olamicolo@gmail.com>
2023-06-16 06:36:31 -0400

    made created_by a charfield in serializers.py

--- backend/BlogApp/serializers.py
+++ backend/BlogApp/serializers.py
@@ -3,6 +3,7 @@ from .models import BlogPost
 
 #Blogpost serializer --> setting lookup-field to 'slug'
 class BlogPostSerializer(serializers.ModelSerializer):
+    created_by = serializers.CharField()
     class Meta:
         model = BlogPost 
         fields = ['title','slug', 'preface','category','image','content','status','featured','created','created_by',]
