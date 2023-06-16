50bdea6a801b655b5b06592698e1c0a13402cf3e
cletusola <olamicolo@gmail.com>
2023-06-16 06:34:21 -0400

    created footer.js in frontend components

---
+++ frontend/blog/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+const Footer = () => {
+
+    return (
+        <React.Fragment>
+            <p className="copyright">&copy; <i>Axis Blog</i>, 2023</p>
+        </React.Fragment>
+    )
+}
+
+export default Footer;
